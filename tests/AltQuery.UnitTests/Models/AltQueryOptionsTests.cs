d2335000d95ba781f9368d1f4ea42a6dc167bc9a
bradford.foxworthhill@everfluxstudios.com <bradford.foxworthhill@everfluxstudios.com>
2019-02-03 13:07:46 -0500

    Writing some tests for GetAltQueryOptions and for the OptionModel

---
+++ tests/AltQuery.UnitTests/Models/AltQueryOptionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using AltQuery.Models.Configuration;
+using FluentAssertions;
+using Xunit;
+
+namespace AltQuery.UnitTests.Models
+{
+    public class AltQueryOptionsTests
+    {
+        // *REMINDER* When Adding additional public properties to AltQueryOptions remember to update code and tests where it is used.
+
+        [Fact]
+        public void AltQueryOptions_Property_Count_Test()
+        {
+            //Arrange
+            // Act
+            var props = new AltQueryOptions().GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+            // Assert
+            props.Should().HaveCount(5);
+        }
+    }
+}
